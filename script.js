99d1e4ae2ed4f6c4bbf3303a6d39939a2a05cc9f
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2022-10-13 17:39:09 +0200

    feat: cells now changing color to black on hover

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const GRID_SIZE = 16;
-const GRID_WIDTH = Math.floor(0.8 * window.visualViewport.height);
+const CELL_WIDTH = Math.floor((0.8 * window.visualViewport.height) / GRID_SIZE);
+const GRID_WIDTH = GRID_SIZE * (CELL_WIDTH + 2);
 
 const gridBox = document.querySelector("section.grid");
 gridBox.style.width = `${GRID_WIDTH}px`;
 gridBox.style.height = `${GRID_WIDTH}px`;
+gridBox.addEventListener("mouseover", changeColor, gridBox);
 
-const cells = populateGridBox(gridBox, GRID_SIZE, GRID_WIDTH);
+populateGridBox(gridBox, GRID_SIZE, CELL_WIDTH);
 
-function populateGridBox(gridElement, size, gridWidth) {
+function populateGridBox(gridElement, size, cellSize) {
 	const cells = [];
 
 	for (let y = 0; y < size; y++) {
 		cells[y] = [];
 		for (let x = 0; x < size; x++) {
 			const cell = document.createElement("div");
-			const cellSize = Math.floor(gridWidth / size);
 			cell.className = "cell";
 			cell.style.width = `${cellSize}px`;
 			cell.style.height = `${cellSize}px`;
 			cells[y][x] = cell;
 			gridElement.appendChild(cell);
 		}
+	}
+	return cells;
+}
+
+function changeColor(event) {
+	const cell = event.target;
+	if (cell != gridBox) {
+		cell.style.backgroundColor = "black";
 	}
 }
