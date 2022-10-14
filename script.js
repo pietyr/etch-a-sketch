09460e3952415b05f437187d4b3d8949ee40ccd3
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2022-10-14 16:38:05 +0200

    refactor: changed grid mechanism to class

--- script.js
+++ script.js
@@ -1,11 +1,64 @@
-let gridSize = 16;
-let cellWidth = Math.floor((0.8 * window.visualViewport.height) / gridSize);
-let gridWidth = gridSize * (cellWidth + 2);
+"use strict";
 
-const gridBox = document.querySelector("section.grid");
-gridBox.style.width = `${gridWidth}px`;
-gridBox.style.height = `${gridWidth}px`;
-gridBox.addEventListener("mouseover", changeColor);
+class Grid {
+	#size;
+	#cells;
+	#gridContainerElement;
+
+	constructor(size = 16) {
+		this.#size = size;
+		this.#cells = [];
+		this.#gridContainerElement = document.querySelector("section.grid");
+		this.#updateGridSize();
+		this.#populateGrid();
+		this.#gridContainerElement.addEventListener("mouseover", (event) => {
+			const cell = event.target;
+			if (cell != this.#gridContainerElement) {
+				cell.style.backgroundColor = "black";
+			}
+		});
+	}
+
+	get cellWidth() {
+		return Math.floor((0.8 * window.visualViewport.height) / this.#size);
+	}
+
+	get gridWidth() {
+		return this.#size * this.cellWidth;
+	}
+
+	changeSize(newSize) {
+		this.#size = newSize;
+		this.#clearGrid();
+		this.#updateGridSize();
+		this.#populateGrid();
+	}
+
+	#populateGrid() {
+		for (let i = 0; i < this.#size ** 2; i++) {
+			const cell = document.createElement("div");
+			cell.className = "cell";
+			cell.style.width = `${this.cellWidth}px`;
+			cell.style.height = `${this.cellWidth}px`;
+			this.#cells[i] = cell;
+			this.#gridContainerElement.appendChild(cell);
+		}
+	}
+
+	#clearGrid() {
+		for (let cell of this.#cells) {
+			cell.remove();
+		}
+		this.#cells = [];
+	}
+
+	#updateGridSize() {
+		this.#gridContainerElement.style.width = `${this.gridWidth}px`;
+		this.#gridContainerElement.style.height = `${this.gridWidth}px`;
+	}
+}
+
+const grid = new Grid();
 
 const dialogElement = document.querySelector("dialog.set-size");
 const changeSizeButton = document.querySelector("button.change-size");
@@ -14,39 +67,15 @@ changeSizeButton.addEventListener("click", openDialog);
 const confirmSizeButton = document.querySelector("button.confirm-size");
 confirmSizeButton.addEventListener("click", confirmSize);
 
-populateGridBox(gridBox, gridSize, cellWidth);
-
-function populateGridBox(gridElement, size, cellSize) {
-	const cells = [];
-
-	for (let y = 0; y < size; y++) {
-		cells[y] = [];
-		for (let x = 0; x < size; x++) {
-			const cell = document.createElement("div");
-			cell.className = "cell";
-			cell.style.width = `${cellSize}px`;
-			cell.style.height = `${cellSize}px`;
-			cells[y][x] = cell;
-			gridElement.appendChild(cell);
-		}
-	}
-	return cells;
-}
-
-function changeColor(event) {
-	const cell = event.target;
-	if (cell != gridBox) {
-		cell.style.backgroundColor = "black";
-	}
-}
-
 function openDialog(event) {
 	dialogElement.showModal();
 }
 
 function confirmSize(event) {
-	// const input = document.querySelector("")
-	gridSize = 16;
-	cellWidth = Math.floor((0.8 * window.visualViewport.height) / gridSize);
-	gridWidth = gridSize * (cellWidth + 2);
+	const input = document.querySelector("input.size-input");
+	const inputValue = Number(input.value);
+	if (Number.isSafeInteger(inputValue)) {
+		grid.changeSize(inputValue);
+		dialogElement.close();
+	}
 }
